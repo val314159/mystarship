aedb118d530ddd7f277b55499fb8d08a18b52898
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-12-14 13:40:20 -0800

    better docs

--- env.sh
+++ env.sh
@@ -1,3 +1,14 @@
 alias wsloop="while true; do echo RESTART ; python -mmystarship.ws ; done"
 
+install () {
+  if [ -d .v ]
+  then
+    echo .v exists
+  else
+    virtualenv .v
+  fi
+  . .v/bin/activate
+  pip install -r requirements.txt
+}
+
 . .v/bin/activate
--- mystarship.py
+++ mystarship.py
@@ -1,29 +1,11 @@
+"""MyStarship Integrated Web Environment (IWE)
+
+It's a web backend with the ability to edit files and manage processes
+live in your browser.  Uses a nice async websocket backend.
+
+"""
 import os, sys, json, traceback as tb
 def fwrite(f,value): f.write(value); return f
-
-import bottle
-app = bottle.default_app()
-
-StaticRoot = os.environ.get('STATIC_ROOT','static')
-
-@app.route('/static/<path:path>')
-def server_static(path):return bottle.static_file(path,root=StaticRoot)
-
-@app.route('/')
-def index():return server_static('x.html')
-
-@app.route('/ws')
-def ws():
-    s=Session(bottle.request.environ["wsgi.websocket"])
-    try:
-        while s._dispatch_message(): pass
-    except:
-        print '*'*80
-        tb.print_exc()
-        print '*'*80
-        s.ws.send(dict(error=True,stacktrace=tb.format_exc().split('\n')))
-        pass
-    pass
 
 class SessionBase:
     def __init__(_,ws,prefix='json_'):_.ws=ws;_.pfx=prefix
@@ -46,12 +28,23 @@ class SessionBase:
             _.ws.send(json.dumps(ret))
             pass
         return True
+    def _return_error(_):
+        print '*'*80
+        tb.print_exc()
+        print '*'*80
+        _.ws.send(dict(error=True,stacktrace=tb.format_exc().split('\n')))
+        pass
+    def _dispatch_loop(_):
+        while _._dispatch_message(): pass
 
 class FsSessMixin:
-    def json_save(_,name,value):
+    "Mix this in for filesystem management"
+    def json_save(_,name,value,offset=0,partial=False):
+        "save a file (or file data)"
         fwrite(open(name,'w'),value).close()
         return dict(result=['ok'])
-    def json_load(_,name,target='#edit'):
+    def json_load(_,name,target='#edit',offset=0,size=-1):
+        "load a file (or file data)"
         try:
             result=[open(name).read(),target]
         except IOError:
@@ -59,18 +52,71 @@ class FsSessMixin:
             pass
         print "LOAD RESULT", repr(result)
         return dict(result=result)
+
+def unblock(f):
+  "sets file to nonblocked state.  sets proc's stdout/stderr to nonblocked"
+  import os,fcntl,subprocess
+  if type(p)==int:
+      fd=p
+      fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+      fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+      return p
+  elif type(p)==subprocess.Popen:
+      unblock(p.stderr)
+      unblock(p.stdout)
+      return p
+  else:
+      return unblock(f.fileno())
+
 class ProcSessMixin:
+    "Mix this in for process management"
+    Procs = []
     def json_system(_,command,target='#edit',cwd=None):
+        "remote system command.  json_system_async is better."
         import subprocess as sp
         z = sp.Popen(command,shell=True,cwd=cwd,
                      stdout=sp.PIPE,
                      stderr=sp.PIPE).communicate()
         return dict(result=[command]+list(z))
+    def json_killph(_,pid):
+        "kill process head"
+        os.killpg(os.getpgid(int(pid)),9)
+        return dict(result=True)
+    def json_system_async(_,command,target='#edit',cwd=None):
+        "remote system command with async output"
+        import subprocess as sp
+        p = unblock(sp.Popen(command,shell=True,cwd=cwd,
+                             stdout=sp.PIPE,stderr=sp.PIPE))
+        Procs.append(p)
+        return dict(result=[command, repr(p), p.pid, len(Procs)-1])
 
 class Session(SessionBase,FsSessMixin,ProcSessMixin):
+    "Concrete session for managing files and procs"
     pass
 
+####################################################
 
+import bottle
+app = bottle.default_app()
+
+StaticRoot = os.environ.get('STATIC_ROOT','static')
+
+@app.route('/static/<path:path>')
+def web_static(path):
+    "serve up static files"
+    return bottle.static_file(path,root=StaticRoot)
+
+@app.route('/')
+def web_root():
+    "serve up /"
+    return web_static('x.html')
+
+@app.route('/ws')
+def web_ws():
+    "serve up the websocket"
+    s=Session(bottle.request.environ["wsgi.websocket"])
+    try   : s._dispatch_loop()
+    except: s._return_error()
 
 if __name__=='__main__':
     from gevent.pywsgi import WSGIServer
