92a70dbec8c80d9405248286f047f8cb1e913e98
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-12-17 14:43:58 -0800

    more configurable reconnect callback

--- make_service.sh
+++ make_service.sh
@@ -1,13 +1,23 @@
 SERVICE_NAME=$1
 PORT=$2
 
+mkdir static
 mkdir $SERVICE_NAME
 mkdir $SERVICE_NAME/static
-echo "hello there" >$SERVICE_NAME/static/index.html
-echo "import mystarship" >$SERVICE_NAME/service.py
-echo "PORT=$PORT python -m mystarship" >run_$SERVICE_NAME.sh
+ln -s $SERVICE_NAME/static static/$SERVICE_NAME
+
+echo "PORT=$PORT python -m$SERVICE_NAME $*" >run_$SERVICE_NAME.sh
+
+cat >$SERVICE_NAME/static/index.html <<EOF
+<doctype html>
+<html>
+<body>
+Hello
+</body>
+</html>
+EOF
 
 cat >$SERVICE_NAME/service.py <<EOF
 import mystarship
-if __name__=='__main__': launch()
+if __name__=='__main__': mystarship.launch()
 EOF
